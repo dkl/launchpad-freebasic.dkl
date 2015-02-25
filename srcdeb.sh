53d7db94de421259c6f52e1c544e64fabaf7ad60
dkl <daniel.c.klauer@web.de>
2015-02-25 19:28:49 +0100

    freebasic-bootstrap: Let to-be-packaged fbc version precompile itself

--- srcdeb.sh
+++ srcdeb.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 set -ex
 
-if [ -z "$FBC" ]; then
-	FBC=fbc
-fi
-
 # Get fbc sources if not yet done
 if [ ! -d fbc ]; then
 	git clone https://github.com/freebasic/fbc.git
@@ -24,18 +20,31 @@ srcdir=freebasic-bootstrap-1.01.0
 tarname=freebasic-bootstrap_1.01.0.orig.tar.gz
 
 # Get a fresh fbc source tree for the wanted git commit
-if [ ! -d $srcdir ]; then
-	cp -R fbc $srcdir
-	cd $srcdir
-	git reset --hard $FBC_GIT
-	rm -rf .git
-	cd ..
+rm -rf $srcdir
+cp -R fbc $srcdir
 
-	# Precompile compiler sources to linux-x86 .asm
-	cd $srcdir
-	$FBC src/compiler/*.bas -m fbc -e -target linux-x86 -r -v
-	cd ..
-fi
+cd $srcdir
+git reset --hard $FBC_GIT
+rm -rf .git
+cd ..
+
+# Use the host fbc to build this FB version, then we can use this bin/fbc to
+# precompile its own sources.
+# This way we ensure that the precompiled sources are compatible with the rtlib
+# from this version, and we don't have to depend on the host fbc's version.
+# But we build in a separate dir to avoid polluting the main source tree.
+tempbuilddir=${srcdir}-tempbuild
+cp -R $srcdir $tempbuilddir
+
+cd $tempbuilddir
+make
+cd ..
+
+cd $srcdir
+../$tempbuilddir/bin/fbc -i ../$tempbuilddir/inc src/compiler/*.bas -m fbc -e -r -v
+cd ..
+
+rm -rf $tempbuilddir
 
 # Make "upstream" tarball
 rm -f $tarname
@@ -48,3 +57,5 @@ cp -R debian/ $srcdir
 cd $srcdir
 debuild
 cd ..
+
+rm -rf $srcdir
