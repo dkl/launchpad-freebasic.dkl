499efe1ae76e4d8bccb91d1b2e9a31dd9b656db6
dkl <daniel.c.klauer@web.de>
2015-02-27 21:25:53 +0100

    freebasic-bootstrap 1.01.0 ppa3: Fix x86_64 build

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ex
 
-dput --config dput-dkl.cf ppa-freebasic.dkl freebasic-bootstrap-x86_1.01.0-0ppa2~utopic1_source.changes
-dput --config dput-dkl.cf ppa-freebasic.dkl freebasic-bootstrap-x86-64_1.01.0-0ppa2~utopic1_source.changes
+dput --config dput-dkl.cf ppa-freebasic.dkl freebasic-bootstrap-x86_1.01.0-0ppa3~utopic1_source.changes
+dput --config dput-dkl.cf ppa-freebasic.dkl freebasic-bootstrap-x86-64_1.01.0-0ppa3~utopic1_source.changes
