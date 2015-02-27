92b341a136064a9e58567cefcfd8a464c3b4523a
dkl <daniel.c.klauer@web.de>
2015-02-27 19:58:03 +0100

    Add config.sh to hold common options

--- freebasic-bootstrap-x86.sh
+++ freebasic-bootstrap-x86.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-set -ex
 
-FBC_VERSION=1.01.0
-FBC_GIT=f67df69261dc87921ef0a375675c359654415418
 FBC_BOOTSTRAP_FBARCH=x86
 FBC_BOOTSTRAP_FBDEBARCH=x86
 FBC_BOOTSTRAP_DEBARCH=i386
 
+source config.sh
 source lib/update-fbc-git.sh
 source lib/freebasic-bootstrap.sh
--- freebasic-bootstrap-x86_64.sh
+++ freebasic-bootstrap-x86_64.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-set -ex
 
-FBC_VERSION=1.01.0
-FBC_GIT=f67df69261dc87921ef0a375675c359654415418
 FBC_BOOTSTRAP_FBARCH=x86_64
 FBC_BOOTSTRAP_FBDEBARCH=x86-64
 FBC_BOOTSTRAP_DEBARCH=amd64
 
+source config.sh
 source lib/update-fbc-git.sh
 source lib/freebasic-bootstrap.sh
