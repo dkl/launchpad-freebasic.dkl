62229db34563d11dc5c8f827883de6f12ff1f9b7
dkl <daniel.c.klauer@web.de>
2015-02-27 21:14:13 +0100

    Upload freebasic-bootstrap-x86-64 too

--- upload.sh
+++ upload.sh
@@ -2,3 +2,4 @@
 set -ex
 
 dput --config dput-dkl.cf ppa-freebasic.dkl freebasic-bootstrap-x86_1.01.0-0ppa2~utopic1_source.changes
+dput --config dput-dkl.cf ppa-freebasic.dkl freebasic-bootstrap-x86-64_1.01.0-0ppa2~utopic1_source.changes
