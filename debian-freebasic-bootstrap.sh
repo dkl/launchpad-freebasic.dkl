64935543c703bc800b02425d812f886fd9316d8e
dkl <daniel.c.klauer@web.de>
2015-02-28 16:46:14 +0100

    Update freebasic-bootstrap packaging to use fbc-bootstrap with same version

--- debian-freebasic-bootstrap.sh
+++ debian-freebasic-bootstrap.sh
@@ -14,36 +14,30 @@ cd debian-freebasic-bootstrap
 # Source packages with multiple upstream tarballs are possible, but there is
 # a special naming convention, which treats additional tarballs as "components"
 # of the main tarball:
-#    <source>_<version>.orig-<component>.tar
+#    <source>_<version>.orig-<component>.tar.*
 # (https://wiki.debian.org/Projects/DebSrc3.0)
-#
-# Thus we have:
-#
-#    FreeBASIC-$FBC_VERSION-source.tar.xz
-# => freebasic-bootstrap_$FBC_VERSION.orig.tar.xz
-#
-#    fbc-bootstrap-$FBC_BOOTSTRAP_VERSION.tar.xz
-# => freebasic-bootstrap_$FBC_VERSION.orig-fbc-bootstrap-$FBC_BOOTSTRAP_VERSION.tar.xz
-#
-# The two have to be extracted as
-#    freebasic-bootstrap-$FBC_VERSION/
-#    freebasic-bootstrap-$FBC_VERSION/fbc-bootstrap-$FBC_BOOTSTRAP_VERSION/
 #
 
 origtarball=freebasic-bootstrap_$FBC_VERSION.orig.tar.xz
-componenttarball=freebasic-bootstrap_$FBC_VERSION.orig-fbc-bootstrap-$FBC_BOOTSTRAP_VERSION.tar.xz
-
 cp ../src/FreeBASIC-$FBC_VERSION-source.tar.xz $origtarball
-cp ../src/fbc-bootstrap-$FBC_BOOTSTRAP_VERSION.tar.xz $componenttarball
 
-# Extract everything and create package source tree
-# The main FB source tarball has a toplevel named FreeBASIC-$FBC_VERSION-source,
-# we rename that to something matching our orig tarball name.
+componenttarball=freebasic-bootstrap_$FBC_VERSION.orig-fbc-bootstrap.tar.xz
+cp ../src/fbc-bootstrap-$FBC_VERSION.tar.xz $componenttarball
+
+#
+# Extract the two tarballs to form one source tree:
+# 1. extract to FreeBASIC-$FBC_VERSION-source/
+#     rename to freebasic-bootstrap-$FBC_VERSION/
+#
+# 2. extract to freebasic-bootstrap-$FBC_VERSION/fbc-bootstrap-$FBC_VERSION/
+#     rename to freebasic-bootstrap-$FBC_VERSION/fbc-bootstrap/
+#
 srcdir=freebasic-bootstrap-$FBC_VERSION
 tar xf $origtarball
 mv FreeBASIC-$FBC_VERSION-source $srcdir
 cd $srcdir
 tar xf ../$componenttarball
+mv fbc-bootstrap-$FBC_VERSION fbc-bootstrap
 cd ..
 
 # Insert debian/ stuff
