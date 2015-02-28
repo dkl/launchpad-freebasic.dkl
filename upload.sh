5ac4a7ea9da03697295f1fe94055a01c1cf2d5a7
dkl <daniel.c.klauer@web.de>
2015-02-28 17:00:27 +0100

    Update upload script for the unified bootstrap package

--- upload.sh
+++ upload.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -ex
 
-dput --config dput-dkl.cf ppa-freebasic.dkl freebasic-bootstrap-x86_1.01.0-0ppa3~utopic1_source.changes
-dput --config dput-dkl.cf ppa-freebasic.dkl freebasic-bootstrap-x86-64_1.01.0-0ppa3~utopic1_source.changes
+dput --config dput-dkl.cf ppa-freebasic.dkl debian-freebasic-bootstrap/freebasic-bootstrap_1.01.0-0ppa1~utopic1_source.changes
