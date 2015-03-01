a978d9e9856418856e1c2f8371aa04f04c3b4a77
dkl <daniel.c.klauer@web.de>
2015-03-01 14:44:15 +0100

    freebasic-bootstrap: Exclude unnecessary files

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 
-dput --config dput-dkl.cf ppa-freebasic.dkl debian-freebasic-bootstrap/freebasic-bootstrap_1.01.0-0ppa1~utopic1_source.changes
+dput --config dput-dkl.cf ppa-freebasic.dkl freebasic-bootstrap/freebasic-bootstrap_1.01.0-0ppa2~utopic1_source.changes
