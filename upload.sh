b684b5dfc60c657fcc54101ea347ff1722c78a18
dkl <daniel.c.klauer@web.de>
2015-05-07 17:58:22 +0200

    upload: 1.02.1-0ppa6: bootstrap in each Ubuntu version

--- upload.sh
+++ upload.sh
@@ -5,5 +5,5 @@ source config.sh
 
 for ubuntuseries in $allubuntuseries; do
 	dput --config dput-dkl.cf ppa-freebasic.dkl \
-		build/${ubuntuseries}/freebasic_${FBVERSION}-0ppa5~${ubuntuseries}1_source.changes
+		build/${ubuntuseries}/freebasic_${FBVERSION}-0ppa6~${ubuntuseries}1_source.changes
 done
